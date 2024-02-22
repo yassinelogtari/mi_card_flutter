a4a31b1f7a4f6f2e32169b2f4eea6d0ada05d3bb
logtari <yassinelogtari@isamm.u-manouba.tn>
2024-02-22 21:15:27 +0100

    images

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MyApp(),
+   const MyApp(),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                height: 100,
-                width: 100,
-                margin: EdgeInsets.only(left:30),
-                padding: EdgeInsets.all(20),
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                margin: EdgeInsets.only(left:30),
-                padding: EdgeInsets.all(20),
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                margin: EdgeInsets.only(left:30),
-                padding: EdgeInsets.all(20),
-                color: Colors.red,
-                child: Text('Container 3'),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/yassine.jpg'),
               ),
             ],
           ),
