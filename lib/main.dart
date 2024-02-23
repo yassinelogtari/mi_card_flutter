ee77466aeae5cd37a1c35c4ebd0df4925a56c175
logtari <yassinelogtari@isamm.u-manouba.tn>
2024-02-23 17:23:16 +0100

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-   const MyApp(),
+    const MyApp(),
   );
 }
 
@@ -16,17 +16,72 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage('images/yassine.jpg'),
               ),
-              Text('Yassine Logtari',
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-              ),)
+              Text(
+                'Yassine Logtari',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'MOBILE DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSans3',
+                  fontSize: 20,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.tealAccent,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child:ListTile(
+                  leading:Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+216 12345678',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'SourceSans3',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child:ListTile(
+                  leading:Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'yassine123@gmail.com',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'SourceSans3',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+
             ],
           ),
         ),
